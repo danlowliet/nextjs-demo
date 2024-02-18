d225c7c27b74b57ae82bce4e3aea3f45fbcad5c9
JediMonkeyDan <109318052+JediMonkeyDan@users.noreply.github.com>
2024-02-18 10:42:19 -0800

    Navbar component and layout

---
+++ components/Navbar.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from 'react'
+
+const links = [
+    { href: 'client', label: 'client'},
+    { href: 'about', label: 'about'},
+    { href: 'contact', label: 'contact'},
+    { href: 'drinks', label: 'drinks'},
+    { href: 'prisma-example', label: 'prisma-example'},
+    { href: 'query', label: 'react-query'},
+    { href: 'tasks', label: 'tasks'}
+]
+
+// Navbar component that renders navigation links
+const Navbar = () => {
+  return (
+    <nav className="bg-base-300 py-4">
+        <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
+            <Link href={"/"} className="btn btn-primary">Next.js</Link>
+            <ul className="menu menu-horizontal md:ml-8">
+                {links.map((link)=> {
+                    return (
+                        <li key={link.href}>
+                            <Link href={link.href} className="capitalize">
+                                {link.label}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
